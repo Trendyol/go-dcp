fc2a9f90471f7b47d79ff906e888e950a19dc37b
Eray Arslan <relfishere@gmail.com>
2022-10-19 16:22:29 +0300

    Unit test improvement

--- dcp_test.go
+++ dcp_test.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/couchbase/gocbcore/v10"
 	"github.com/stretchr/testify/assert"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
+	"strings"
+	"sync"
 	"testing"
 	"time"
 )
@@ -38,23 +41,39 @@ func insertDataToContainer(t *testing.T, config Config) {
 	_ = client.Connect()
 	defer client.Close()
 
-	opm := newAsyncOp(nil)
-	op, err := client.GetAgent().Set(gocbcore.SetOptions{
-		Key:   []byte("my_key"),
-		Value: []byte("Some value"),
-	}, func(result *gocbcore.StoreResult, err error) {
-		opm.Resolve()
-	})
+	wg := sync.WaitGroup{}
+	wg.Add(1000)
 
-	if err != nil {
-		t.Error(err)
+	for i := 0; i < 1000; i++ {
+		go func(i int) {
+			ch := make(chan error)
+
+			opm := newAsyncOp(nil)
+			op, err := client.GetAgent().Set(gocbcore.SetOptions{
+				Key:   []byte(fmt.Sprintf("my_key_%v", i)),
+				Value: []byte(fmt.Sprintf("my_value_%v", i)),
+			}, func(result *gocbcore.StoreResult, err error) {
+				ch <- err
+				opm.Resolve()
+			})
+
+			if err != nil {
+				t.Error(err)
+			}
+
+			err = <-ch
+
+			err = opm.Wait(op, err)
+
+			if err != nil {
+				t.Error(err)
+			}
+
+			wg.Done()
+		}(i)
 	}
 
-	err = opm.Wait(op, err)
-
-	if err != nil {
-		t.Error(err)
-	}
+	wg.Wait()
 }
 
 func TestDcp(t *testing.T) {
@@ -69,15 +88,27 @@ func TestDcp(t *testing.T) {
 	var dcp Dcp
 	var err error
 
+	counter := 0
+	lock := sync.Mutex{}
+
 	dcp, err = NewDcp(configPath, func(event int, data interface{}, err error) {
 		if err != nil {
 			return
 		}
 
 		if event == MutationName {
+			lock.Lock()
+			counter++
+			lock.Unlock()
+
 			mutation := data.(gocbcore.DcpMutation)
-			assert.Equal(t, string(mutation.Key), "my_key")
-			dcp.Close()
+
+			assert.True(t, strings.HasPrefix(string(mutation.Key), "my_key"))
+			assert.True(t, strings.HasPrefix(string(mutation.Value), "my_value"))
+
+			if counter == 1000 {
+				dcp.Close()
+			}
 		}
 	})
 

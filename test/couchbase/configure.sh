3dc4f6b8d18cfd1d33d69a2c6e138864a6b2c40f
mhmtszr <m.szr98@gmail.com>
2023-07-19 14:37:35 +0300

    Update Couchbase cluster init

--- test/couchbase/configure.sh
+++ test/couchbase/configure.sh
@@ -29,8 +29,12 @@ until [[ $(check_db) = 0 ]]; do
   sleep 1
 done
 
-echo "cbloader"
-cbdocloader -c couchbase://127.0.0.1 -u $USERNAME -p $PASSWORD -m 1024 -b dcp-test -d /opt/couchbase/samples/travel-sample.zip
+couchbase-cli cluster-init -c localhost --cluster-name Cluster --cluster-username user \
+  --cluster-password 123456 --services data --cluster-ramsize 1024
+
+couchbase-cli bucket-create -c couchbase --username user --password 123456 --bucket dcp-test --bucket-type couchbase --bucket-ramsize 1024
+
+cbimport json -c couchbase://127.0.0.1 -u user -p 123456 --bucket-quota 1024 -b dcp-test -d file://opt/couchbase/samples/travel-sample.zip -f sample
 
 echo "couchbase-dev started"
 

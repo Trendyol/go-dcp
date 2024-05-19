6c5264c35dac3443cd376564137a2287e5b97823
ozgeonay <ozgeony@gmail.com>
2024-05-19 21:16:40 +0300

    feat: config environment variables replacement implemented. (#97)

--- dcp_test.go
+++ dcp_test.go
@@ -251,3 +251,43 @@ func TestDcp(t *testing.T) {
 		test(t, version)
 	})
 }
+
+func TestNewDcpConfigWithEnvVariables(t *testing.T) {
+	os.Setenv("DCP_USERNAME", "envUser")
+	os.Setenv("DCP_PASSWORD", "envPass")
+	os.Setenv("DCP_BUCKET_NAME", "envBucket")
+
+	configContent := `
+hosts: ["localhost:8091"]
+username: ${DCP_USERNAME}
+password: ${DCP_PASSWORD}
+bucketName: ${DCP_BUCKET_NAME}
+`
+	tmpFile, err := os.CreateTemp("", "dcpConfig-*.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(tmpFile.Name())
+
+	if _, err := tmpFile.Write([]byte(configContent)); err != nil {
+		t.Fatal(err)
+	}
+	if err := tmpFile.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	dcpConfig, err := newDcpConfig(tmpFile.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if dcpConfig.Username != "envUser" {
+		t.Errorf("expected username to be 'envUser', got '%s'", dcpConfig.Username)
+	}
+	if dcpConfig.Password != "envPass" {
+		t.Errorf("expected password to be 'envPass', got '%s'", dcpConfig.Password)
+	}
+	if dcpConfig.BucketName != "envBucket" {
+		t.Errorf("expected bucketName to be 'envBucket', got '%s'", dcpConfig.BucketName)
+	}
+}
